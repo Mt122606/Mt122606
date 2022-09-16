ec7ebb550b71b14d84d438c152dd7106b0f1ee90
Mt122606 <108757589+Mt122606@users.noreply.github.com>
2022-09-16 09:07:18 -0700

    Add files via upload

---
+++ Cox_Cover.py
@@ -0,0 +1,5 @@
+print("Hello Cox")
+about_Me = """ \t I have been a contractor for more than a year working for 3i People. I am found to be fast and readily available.
+\n \tI am glad to have to opportunty to learn and grow while working for you. I value the work/life balance that I have achieved. 
+\n \tPeople have been known to come to me for issues they cateragoragize as 'wonkie' for my understanding and solutions.  """
+print(about_Me)
