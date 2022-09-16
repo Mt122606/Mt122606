fdeb7cb44f81b9f924d2fdea3be6be0b3dffacf5
Mt122606 <108757589+Mt122606@users.noreply.github.com>
2022-09-16 12:57:15 -0700

    Update Cox_Cover.py

--- Cox_Cover.py
+++ Cox_Cover.py
@@ -1,5 +1,7 @@
-print("Hello Cox")
+print("Hello!")
 about_Me = """ \t I have been a contractor for more than a year working for 3i People. I am found to be fast and readily available.
-\n \tI am glad to have to opportunty to learn and grow while working for you. I value the work/life balance that I have achieved. 
-\n \tPeople have been known to come to me for issues they cateragoragize as 'wonkie' for my understanding and solutions.  """
+\n \tI am glad to have to opportunty to learn and grow while working for COX. I value the work/life balance that I have achieved. 
+\n \tPeople have been known to come to me for issues they cateragoragize as 'wonkie' for my understanding and solutions. 
+\n \tI want to work full time as an employee, no longer as contract employee. I want to be and provide more value with added responsibility to productively complete assigned projects. """
 print(about_Me)
+
